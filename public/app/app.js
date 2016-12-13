42b27875e38926d0ce599a07b16eb052004dda7b
zwb1988 <zwb1988@hotmail.com>
2016-12-13 20:24:25 +0800

    Completed session - Your first directive

--- public/app/app.js
+++ public/app/app.js
@@ -3,5 +3,26 @@
 
     app.controller('mainCtrl', function () {
         var vm = this;
+        vm.user = {
+            name: 'Luke Skywalker',
+            address: {
+                street: 'PO Box 123',
+                city: 'Secret Rebel Base',
+                planet: 'Yavin 4'
+            },
+            friends: [
+                'Han',
+                'Leia',
+                'Chewbacca'
+            ]
+        };
+    });
+
+    app.directive('userInfoCard', function () {
+        return {
+            restrict: 'E',
+            templateUrl: '/app/templates/userInfoCard.html',
+            replace: false //a root html element is required in the template
+        };
     });
 }());
