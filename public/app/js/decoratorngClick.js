ec6ece3c0391433e221354cc18954a16ac953809
zwb1988 <zwb1988@hotmail.com>
2016-12-15 00:20:35 +0800

    Decorator directive - recreating ngClick

---
+++ public/app/js/decoratorngClick.js
@@ -0,0 +1,27 @@
+(function () {
+    var app = angular.module('app');
+
+    app.controller('mainCtrlngClick', function ($scope) {
+        $scope.data = {
+            message: 'I have not been clicked'
+        };
+
+        $scope.clickHandler = function (p) {
+            p.message = 'I have been clicked';
+        };
+    });
+
+    app.directive('myClick', function ($parse) {
+        return {
+            link: function (scope, element, attributes, controller) {
+                var fn = $parse(attributes['myClick']);
+                element.on('click', function (event) {
+                    scope.$apply(function () {
+                        fn(scope);
+                    });
+                    fn();
+                });
+            }
+        };
+    });
+}());
