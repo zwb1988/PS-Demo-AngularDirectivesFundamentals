84656ba2fe8f8eb559e7748a136f1e4c45f0b4fd
zwb1988 <zwb1988@hotmail.com>
2016-12-15 00:55:48 +0800

    Business specific decorator

--- public/app/js/decoratorBusinessSpecific.js
+++ public/app/js/decoratorBusinessSpecific.js
@@ -2,6 +2,30 @@
     var app = angular.module('app');
 
     app.controller('mainCtrlngBusinessSpecific', function ($scope) {
-        
+        $scope.user1 = {
+            name: "Luke",
+            selected: false
+        };
+    });
+
+    app.directive('userTile', function () {
+        return {
+            restrict: 'E',
+            scope: {
+                user: '='
+            },
+            templateUrl: '/app/templates/userTile.html'
+        };
+    });
+
+    app.directive('userClickSelect', function () {
+        return {
+            link: function (scope, element, attributes) {
+                element.on('click', function () {
+                    scope.user.selected = !scope.user.selected;
+                    scope.$apply();
+                });
+            }
+        };
     });
 }());
